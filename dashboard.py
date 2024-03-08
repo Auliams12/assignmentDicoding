b6739f235eff72b66ecef9e4884ddf5b7d1ba603
Auliams12 <161486892+Auliams12@users.noreply.github.com>
2024-03-08 07:42:16 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data from the file day_aul
-day_aul = pd.read_csv('C:/Users/user/Downloads/assignment_dashboard_analisis/dashboard/all_data.csv')  # Ganti 'day_aul.csv' dengan nama file yang sesuai
+day_aul = pd.read_csv('C:/Users/user/Downloads/assignment_dashboard_analisis/dashboard/all_data.csv')
+
 
 # Streamlit App
 st.title('Dashboard Analisis Pengguna Sepeda')
